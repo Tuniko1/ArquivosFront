5670c5d3753dbe4b67f5eee844ddfd702aeee818
Arthur <arthurcaco@gmail.com>
2023-07-13 21:53:45 -0300

    JS Básico

---
+++ basico_JS/js/basico.js
@@ -0,0 +1,21 @@
+console.log("hello world, arquivo js");
+
+console.log (1 + "1");
+console.log (1 - "1");
+
+var x = 10
+let y = 10
+
+var x = "ar"
+console.log(typeof(x))
+
+console.log("aula" * 2)
+
+let numero = 50
+let numeroString = String(50)
+let casasDecimais = 10.9888
+
+
+console.log(typeof(numero))
+console.log(typeof(numeroString))
+console.log(typeof(casasDecimais))
